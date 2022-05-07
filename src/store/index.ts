6cc133372c1d5c3793e6d3f0b854bdfd272826bb
Higor Allan <higor.allan21@gmail.com>
2022-05-07 12:21:35 -0300

    Study about Redux-saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,19 @@
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import rootReducer from './modules/rootReducer';
 import { ICartState } from './modules/cart/types';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './modules/rootSaga';
 
 export interface IState {
 	cart: ICartState;
 }
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
